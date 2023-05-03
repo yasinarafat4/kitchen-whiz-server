105cab3f2c7e6e6a7ec8ff0701069e5b0ed87455
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-05-03 06:32:47 +0600

    accessing specific chefs by id is done

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
-// const cors = require("cors");
+const cors = require("cors");
 const port = process.env.PORT || 5000;
+
+const chefs = require("./data/chef.json");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Chef is running!");
+});
+
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
+});
+
+app.get("/chefs/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedChefs = chefs.find((chef) => chef._id === id);
+  res.send(selectedChefs);
 });
 
 app.listen(port, () => {
